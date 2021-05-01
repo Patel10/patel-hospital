7cd3c824e40575cbb124ef6d72cafabaac0ad666
Parth <81577434+Patel10@users.noreply.github.com>
2021-05-01 18:28:53 -0400

    Initial commit

---
+++ src/main/java/project/App.java
@@ -0,0 +1,11 @@
+package project;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+}
